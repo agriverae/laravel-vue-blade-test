fba73050997914ce064871ad3efc91192b662737
Ali Rivera <user@MAC-0280.fibertel.com.ar>
2021-01-11 09:50:04 -0300

    first commit

---
+++ resources/js/components/TestComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Hola</h1>
+        {{ message }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["message"],
+    mounted() {
+        console.log("Component mounted.");
+    }
+};
+</script>
